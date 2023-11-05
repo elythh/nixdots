81126618dd5b69d3f196cbfdc0b6d48469a3cb37
elythh <gwenchlan@tuta.io>
2023-11-05 14:08:33 +0100

    chore: read the diff :)

---
+++ theme/colors/stardew.lua
@@ -0,0 +1,16 @@
+local M = {}
+M.name  = 'stardew'
+M.ok    = "#55b682"
+M.warn  = "#ff9c6a"
+M.err   = "#f25c5c"
+M.pri   = "#7aaaff"
+M.dis   = "#f17ac6"
+M.bg    = "#16161a"
+M.mbg   = "#16161a"
+M.bg3   = "#222629"
+M.bg4   = "#262b30"
+M.fg    = "#F8F9FA"
+M.fg2   = "#e8F9FA"
+M.fg3   = "#eeeff0"
+M.fg4   = "#515c68"
+return M
