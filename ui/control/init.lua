94cdb435798fab0ef2815b8f6236d29c73451b73
elythh <gwenchlan@tuta.io>
2023-11-15 10:45:37 +0100

    fix(ui): multiple monitors

--- ui/control/init.lua
+++ ui/control/init.lua
@@ -9,81 +9,79 @@ local buttons = require("ui.control.mods.buttons")
 local sliders = require("ui.control.mods.slider")
 local footer = require("ui.control.mods.footer")
 
-awful.screen.connect_for_each_screen(function(s)
-  local control = wibox({
-    shape = helpers.rrect(12),
-    screen = s,
-    width = 520,
-    height = 440,
-    bg = beautiful.bg .. "00",
-    ontop = true,
-    visible = false,
-  })
+local control = wibox({
+  shape = helpers.rrect(12),
+  screen = s,
+  width = 520,
+  height = 440,
+  bg = beautiful.bg .. "00",
+  ontop = true,
+  visible = false,
+})
 
-  control:setup {
-    nil,
+control:setup {
+  nil,
+  {
     {
       {
         {
-          {
 
+          {
+            layout = wibox.layout.align.horizontal,
             {
-              layout = wibox.layout.align.horizontal,
-              {
-                font = beautiful.sans .. " 14",
-                markup = helpers.colorizeText("Control Center", beautiful.fg),
-                widget = wibox.widget.textbox,
-                valign = "center",
-                align = "center"
-              },
-              nil,
+              font = beautiful.sans .. " 14",
+              markup = helpers.colorizeText("Control Center", beautiful.fg),
+              widget = wibox.widget.textbox,
+              valign = "center",
+              align = "center"
+            },
+            nil,
+            {
               {
                 {
-                  {
-                    font = beautiful.icon .. " 12",
-                    markup = helpers.colorizeText("󰐱", beautiful.fg),
-                    widget = wibox.widget.textbox,
-                    valign = "center",
-                    align = "center"
-                  },
-                  widget = wibox.container.margin,
-                  margins = 10
+                  font = beautiful.icon .. " 12",
+                  markup = helpers.colorizeText("󰐱", beautiful.fg),
+                  widget = wibox.widget.textbox,
+                  valign = "center",
+                  align = "center"
                 },
-                widget = wibox.container.background,
-                shape = helpers.rrect(6),
-                buttons = {
-                  awful.button({}, 1, function()
-                    awesome.emit_signal('toggle::setup')
-                  end)
-                },
-                bg = beautiful.mbg
+                widget = wibox.container.margin,
+                margins = 10
               },
+              widget = wibox.container.background,
+              shape = helpers.rrect(6),
+              buttons = {
+                awful.button({}, 1, function()
+                  awesome.emit_signal('toggle::setup')
+                end)
+              },
+              bg = beautiful.mbg
             },
-            widget = wibox.container.margin,
-            top = 18,
-            left = 10,
-            right = 10
           },
-          buttons,
-          sliders,
-          footer,
-          layout = wibox.layout.fixed.vertical,
-          spacing = 20,
+          widget = wibox.container.margin,
+          top = 18,
+          left = 10,
+          right = 10
         },
-        widget = wibox.container.margin,
-        left = 20,
-        right = 20,
+        buttons,
+        sliders,
+        footer,
+        layout = wibox.layout.fixed.vertical,
+        spacing = 20,
       },
-      widget = wibox.container.background,
-      bg = beautiful.bg,
-      shape = helpers.rrect(12),
+      widget = wibox.container.margin,
+      left = 20,
+      right = 20,
     },
-    nil,
-    layout = wibox.layout.align.vertical,
-    spacing = 8,
-  }
-  awful.placement.bottom_right(control, { honor_workarea = true, margins = 20 })
-  awesome.connect_signal("toggle::control", function()
-    control.visible = not control.visible
-  end)
+    widget = wibox.container.background,
+    bg = beautiful.bg,
+    shape = helpers.rrect(12),
+  },
+  nil,
+  layout = wibox.layout.align.vertical,
+  spacing = 8,
+}
+awful.placement.bottom_right(control, { honor_workarea = true, margins = 20 })
+awesome.connect_signal("toggle::control", function()
+  control.visible = not control.visible
 end)
