ef3449fbb0c56f6e151f4dd82765320fc4fd646a
elythh <gwenchlan@tuta.io>
2024-01-08 11:44:09 +0100

    multiple monitors

--- ui/dash/init.lua
+++ ui/dash/init.lua
@@ -16,46 +16,44 @@ local time = require("ui.dash.mods.time")
 local nf = require("ui.dash.mods.nf")
 local weather = require("ui.dash.mods.weather")
 
-awful.screen.connect_for_each_screen(function(s)
-  local dash = wibox({
-    shape = helpers.rrect(12),
-    screen = s,
-    width = 1350,
-    height = 790,
-    bg = beautiful.bg,
-    ontop = true,
-    visible = false,
-  })
+local dash = wibox({
+  shape = helpers.rrect(12),
+  screen = s,
+  width = 1350,
+  height = 790,
+  bg = beautiful.bg,
+  ontop = true,
+  visible = false,
+})
 
-  dash:setup {
+dash:setup {
+  {
     {
-      {
-        profile,
-        pomo,
-        quote,
-        layout = wibox.layout.align.vertical,
-      },
-      {
-        nil,
-        todo,
-        song,
-        layout = wibox.layout.align.vertical,
-      },
-      {
-        time,
-        nf,
-        weather,
-        spacing = 20,
-        layout = wibox.layout.fixed.vertical,
-      },
-      spacing = 20,
-      layout = wibox.layout.flex.horizontal,
+      profile,
+      pomo,
+      quote,
+      layout = wibox.layout.align.vertical,
     },
-    widget = wibox.container.margin,
-    margins = 20,
-  }
-  awful.placement.centered(dash, { honor_workarea = true, margins = 20 })
-  awesome.connect_signal("toggle::dash", function()
-    dash.visible = not dash.visible
-  end)
+    {
+      nil,
+      todo,
+      song,
+      layout = wibox.layout.align.vertical,
+    },
+    {
+      time,
+      nf,
+      weather,
+      spacing = 20,
+      layout = wibox.layout.fixed.vertical,
+    },
+    spacing = 20,
+    layout = wibox.layout.flex.horizontal,
+  },
+  widget = wibox.container.margin,
+  margins = 20,
+}
+awful.placement.centered(dash, { honor_workarea = true, margins = 20 })
+awesome.connect_signal("toggle::dash", function()
+  dash.visible = not dash.visible
 end)
