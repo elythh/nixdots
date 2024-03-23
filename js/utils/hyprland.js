d4a44e83f9a11dd765e17f9048f87e7b74e78c2c
elythh <gwen@omg.lol>
2024-03-23 14:12:58 +0100

    feat(bar): style for empty workspaces

--- js/utils/hyprland.js
+++ js/utils/hyprland.js
@@ -1,12 +1,19 @@
 import { Hyprland } from "../imports.js";
 
 export const getFocusedWorkspace = (self) =>
-    self.children.forEach((btn) => {
-        btn.className =
-            btn.attribute.index === Hyprland.active.workspace.id
-                ? "focused"
-                : "";
-        btn.visible = Hyprland.workspaces.some(
-            (ws) => ws.id === btn.attribute.index,
-        );
-    });
+  self.children.forEach((btn) => {
+    if (
+      Hyprland.getWorkspace(btn.attribute.index)?.windows > 0 &&
+      btn.attribute.index !== Hyprland.active.workspace.id
+    ) {
+      btn.className = "";
+    } else {
+      btn.className =
+        btn.attribute.index === Hyprland.active.workspace.id
+          ? "focused"
+          : "empty";
+    }
+    btn.visible = Hyprland.workspaces.some(
+      (ws) => ws.id === btn.attribute.index,
+    );
+  });
--- js/windows/bar/modules/workspaces.js
+++ js/windows/bar/modules/workspaces.js
@@ -4,22 +4,22 @@ const { Box, Button } = Widget;
 const { messageAsync } = Hyprland;
 
 export const Workspaces = () =>
-    Box({
-        className: "workspaces",
-        child: Box({
-            vertical: true,
-            children: Array.from({ length: 10 }, (_, i) => i + 1).map((i) =>
-                Button({
-                    cursor: "pointer",
-                    attribute: { index: i },
-                    onClicked: () => messageAsync(`dispatch workspace ${i}`),
-                    onSecondaryClick: () =>
-                        messageAsync(`dispatch movetoworkspacesilent ${i}`),
-                }),
-            ),
-
-            setup: (self) => {
-                self.hook(Hyprland, getFocusedWorkspace);
-            },
+  Box({
+    className: "workspaces",
+    child: Box({
+      vertical: true,
+      children: Array.from({ length: 10 }, (_, i) => i + 1).map((i) =>
+        Button({
+          cursor: "pointer",
+          attribute: { index: i },
+          onClicked: () => messageAsync(`dispatch workspace ${i}`),
+          onSecondaryClick: () =>
+            messageAsync(`dispatch movetoworkspacesilent ${i}`),
         }),
-    });
+      ),
+
+      setup: (self) => {
+        self.hook(Hyprland, getFocusedWorkspace);
+      },
+    }),
+  });
