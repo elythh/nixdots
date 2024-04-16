b017d449058801889021769afeb64f5cdb3e3181
elythh <gwen@omg.lol>
2024-04-16 21:36:03 +0200

    :sparkles: bottom bar

--- js/bar/Clock.js
+++ js/bar/Clock.js
@@ -4,16 +4,15 @@ export const Clock = () =>
   Widget.Box({
     class_name: "clock",
     spacing: 12,
-    vertical: true,
     children: [
       Widget.Icon({
         class_name: "icon",
-        icon: "clock",
+        icon: "clock-applet-symbolic",
       }),
       Widget.Label({
         setup: (self) =>
           self.poll(1000, (self) =>
-            execAsync(["date", "+%H%n%M"]).then((date) => (self.label = date)),
+            execAsync(["date", "+%H:%M"]).then((date) => (self.label = date)),
           ),
       }),
     ],
